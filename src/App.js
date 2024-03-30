767bf76bff24cb460d4bdb1a91eb8bc6fff2798f
Mansi6396 <mansitrivedi315@gmail.com>
2024-03-30 14:34:39 +0530

    done

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import { ScrollToTop } from "react-simple-scroll-up";
 import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
 
 function App() {
+  
   return (
     <>
       <div className="App">
